c47bb37ec604590cf191f41ce9f65c6de1ad498d
Victor Praxedes <victor.praxedes@gmail.com>
2018-09-02 22:08:12 -0300

    structured and documented the library's header

--- libs/DHT12-lib/DHT12-lib.cpp
+++ libs/DHT12-lib/DHT12-lib.cpp
@@ -1,6 +1,5 @@
-
 #include <Wire.h>
-#include <DHT12.h>
+#include <DHT12-lib.h>
 
 #ifdef ESP8266
 void DHT12::begin(uint8_t sda, uint8_t scl)
@@ -14,57 +13,24 @@ void DHT12::begin()
   Wire.begin();
 }
 
-int8_t DHT12::read()
+bool DHT12::read()
 {
-  // READ SENSOR
-  int status = _readSensor();
-  if (status < 0) return status;
-
-  // CONVERT AND STORE
-  humidity = bits[0] + bits[1] * 0.1;
-  temperature = bits[2] + (bits[3] & 0x7F) * 0.1;
-  if (bits[3] & 0x80)
-  {
-    temperature = -temperature;
-  }
-
-  // TEST CHECKSUM
-  uint8_t checksum = bits[0] + bits[1] + bits[2] + bits[3];
-  if (bits[4] != checksum)
-  {
-    return DHT12_ERROR_CHECKSUM;
-  }
-
-  return DHT12_OK;
+  return 0;
 }
 
-int DHT12::_readSensor()
+RegisterDht DHT12::read_sensor()
 {
-  /* DHT12 I²C Communication
-  It has 5 internal registers that holds the sensor's information
-  ADDR  R/W  DESCRIPTION
-  0x00   R   Humidity Integer                     
-  0x01   R   Humidity Float Decimals 
-  0x02   R   Temperature Integer                  
-  0x03   R   Temperature Float Decimals                   
-  0x04   R   Checksum                 
 
-  */
-  Wire.beginTransmission(DHT12_ADDRESS);
-  Wire.write(0);
-  int rv = Wire.endTransmission();
-  if (rv < 0) return rv;
+  RegisterDht placeholder;
+  return placeholder;
+}
 
-  // GET DATA
-  const uint8_t length = 5;
-  int bytes = Wire.requestFrom(DHT12_ADDRESS, length);
-  if (bytes == 0) return DHT12_ERROR_CONNECT;
-  if (bytes < length) return DHT12_MISSING_BYTES;
+float DHT12::get_temperature()
+{
+  return 0;
+}
 
-  for (int i = 0; i < bytes; i++)
-  {
-    bits[i] = Wire.read();
-  }
-
-  return bytes;
+float DHT12::get_humidity()
+{
+  return 0;
 }
--- libs/DHT12-lib/DHT12-lib.h
+++ libs/DHT12-lib/DHT12-lib.h
@@ -1,7 +1,27 @@
 /*
 	DHT12 I²C Driver Library
-
 	Based on https://github.com/RobTillaart/Arduino.git
+
+  @author Victor Praxedes
+  @version 0.1 2/9/18
+  
+  # DHT12 Internal Data Registers
+   It has 5 internal 1 byte registers that holds the sensor's information
+      ADDR  R/W  DESCRIPTION
+    - 0x00   R   Humidity Integer                     
+    - 0x01   R   Humidity Float Decimals 
+    - 0x02   R   Temperature Integer                  
+    - 0x03   R   Temperature Float Decimals                   
+    - 0x04   R   Checksum                 
+   
+  # DHT12 I²C Communication
+   DHT12 expects the following byte sequence on the I²C bus
+    - DEVICE ADDRESS > REGISTER ADDRESS
+          (0x5C)        (0x00 - 0x04)
+
+   It will then output the on the I²C bus the data inside its internal
+   registers starting on the address provided on the last transaction
+    - DATA REG 0x00 < DATA REG 0x02 < ... < DATA REG 0x04  
 */
 
 #ifndef DHT12LIB_H
@@ -9,22 +29,66 @@
 
 #define DHT12_ADDRESS  ((uint8_t)0x5C)
 
+struct RegisterDht
+{
+	uint8_t HumitityInteger;
+	uint8_t HumitityFloat;
+	uint8_t TemperatureInteger;
+	uint8_t TemperatureFloar;
+	uint8_t Checksum;
+
+};
+
 class DHT12
 {
 public:
 
-  #ifdef ESP8266
-  void begin(uint8_t sda, uint8_t scl);
-  #endif
+	#ifdef ESP8266
+	/**
+	 * If using ESP8266, you have to declare which pins
+	 * are going to be used for data (SDA) and clock (SCL)
+	 * NOTE: they must have the PWM symbol ~ on the board
+	 * Starts the I²C bus
+	 * 
+	 * @sda pin for data
+	 * @scl pin for clock
+	 */
+	void begin(uint8_t sda, uint8_t scl);
+	#endif
 
-  void begin();
-  int8_t read();
+	/**
+	 * Starts the I²C bus
+	 */
+	void begin();
+
+	/**
+	 * Tells the sensor to get the readings 
+	 * 
+	 * @return true for suscefull reading, false otherwise
+	 */
+	bool read();
+
+	/**
+	 * @return temperature value
+	 */
+	float get_temperature();
+
+	/**
+	 * @return humidity value
+	 */
+	float get_humidity();
 
 private:
-  float humidity;
-  float temperature;
+	/**
+	 * Stores the sensor's last reading
+	 */
+	RegisterDht DhtData;
 
-  int _readSensor();
+	/**
+	 * Gets the sensor's reading using I²C
+	 * @return sensor's registers data as a struct
+	 */
+	RegisterDht read_sensor();
 };
 
 #endif
